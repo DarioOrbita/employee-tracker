ac08807bb7ac0fca3d42d863582c9b23511d3c20
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-25 13:53:26 -0500

    add schema tables

---
+++ db/connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Xlv7qy8922!',
+    database: 'employee_tracker'
+
+})
---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS exployee;
+
+CREATE TABLE department(
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role(
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    CONSTRAINT fk_department 
+        FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id VARCHAR(30) DEFAULT NULL,
+    CONSTRAINT fk_role
+        FOREIGN KEY (role_id) 
+        REFERENCES roles(id) 
+        ON DELETE SET NULL
+);
