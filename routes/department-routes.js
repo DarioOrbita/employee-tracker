bf97dde9c2589e2c61787c8d57430cf361b4f8a6
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-25 15:43:59 -0500

    add post and delete department routes

--- routes/department-routes.js
+++ routes/department-routes.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../db/connection');
+const db = require("../db/connection");
 
-router.get('/departments', (req, res) => {
-    const sql = `SELECT * FROM department`;
-    db.query(sql, (err, rows) => {
-        if (err) {
-          res.status(500).json({ error: err.message });
-          return;
-        }
-        res.json({
-          message: 'success',
-          data: rows
-        });
-      });
+router.get("/departments", (req, res) => {
+  const sql = `SELECT * FROM department`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
 });
 
-router.get('/department/:id', (req, res) => {
-    const sql = `SELECT * FROM department WHERE id = ?`
-    const params = [req.params.id];
+router.get("/department/:id", (req, res) => {
+  const sql = `SELECT * FROM department WHERE id = ?`;
+  const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
     if (err) {
@@ -26,8 +26,43 @@ router.get('/department/:id', (req, res) => {
       return;
     }
     res.json({
-      message: 'success',
-      data: row
+      message: "success",
+      data: row,
     });
   });
-})
+});
+
+router.post("/departments", ({ body }, res) => {
+  const sql = `INSERT INTO department (department_name) VALUES (?)`;
+  const params = [body.name];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});
+
+router.delete("/department/:id", (req, res) => {
+  const sql = `DELETE FROM department WHERE id = ?`;
+  db.query(sql, req.params.id, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Department not found",
+      });
+    } else {
+      res.json({
+        message: "deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
